ef30296e5e1190c539203be7b5457432dfbca5e9
bilal simsek <xyzbilal@gmail.com>
2023-07-20 20:13:24 +0300

    Initial Commit

---
+++ HeroNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeroNavigation
+//
+//  Created by Bilal SIMSEK on 20.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeroNavigation/HeroNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroNavigationApp.swift
+//  HeroNavigation
+//
+//  Created by Bilal SIMSEK on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
